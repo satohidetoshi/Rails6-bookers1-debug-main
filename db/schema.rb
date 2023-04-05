85391a49975b9fbe04c3c800d9996c5701618cee
EC2 Default User <ec2-user@ip-172-31-10-174.ap-northeast-1.compute.internal>
2023-04-05 18:40:19 +0000

    finsh

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_30_081056) do
+ActiveRecord::Schema.define(version: 2023_04_05_183256) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
